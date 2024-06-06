0d58946908fa39d96f4313f0d1cbd00d5e800c29
red-eyed-99 <mgnblack99@gmail.com>
2024-06-06 18:48:47 +0300

    Task31_AddGeyser done

--- ConsoleMaze/ConsoleMaze/Maze/MazeBuilder.cs
+++ ConsoleMaze/ConsoleMaze/Maze/MazeBuilder.cs
@@ -49,6 +49,8 @@ namespace ConsoleMaze.Maze
 
             BuildWeakWall();
 
+            BuildGeyser();
+
             var hero = new Hero(0, 0, maze, 7, 10, 0, 10, 12);
             maze.Hero = hero;
 
@@ -213,6 +215,22 @@ namespace ConsoleMaze.Maze
             {
                 var randomWall = GetRandom(walls);
                 maze[randomWall.X, randomWall.Y] = new WeakWall(randomWall.X, randomWall.Y, maze); 
+            }
+        }
+
+        private void BuildGeyser()
+        {
+            var fountains = maze.Cells.Where(x => x is Fountain).ToList();
+            var groundsNearFountain = new List<Ground>();
+
+            for (int i = 0; i < fountains.Count; i++)
+            {
+                groundsNearFountain.AddRange(GetNear<Ground>(maze[fountains[i].X, fountains[i].Y]));
+            }
+
+            foreach (var ground in groundsNearFountain)
+            {
+                maze[ground.X, ground.Y] = new Puddle(ground.X, ground.Y, maze);
             }
         }
 
