187fd11e3be9103d87264e698b4a9e60b6c91740
red-eyed-99 <mgnblack99@gmail.com>
2024-06-27 10:11:05 +0300

    delete unused usings

--- ConsoleMaze/ConsoleMaze/Maze/Cells/WeakWall.cs
+++ ConsoleMaze/ConsoleMaze/Maze/Cells/WeakWall.cs
@@ -1,9 +1,4 @@
 ﻿using ConsoleMaze.Maze.Cells.Enemies;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleMaze.Maze.Cells
 {
