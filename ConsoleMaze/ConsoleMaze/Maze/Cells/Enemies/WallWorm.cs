55d332b1f5427742d3a24dc0439d32cb7390d969
red-eyed-99 <mgnblack99@gmail.com>
2024-06-27 09:30:57 +0300

    delete unused usings

--- ConsoleMaze/ConsoleMaze/Maze/Cells/Enemies/WallWorm.cs
+++ ConsoleMaze/ConsoleMaze/Maze/Cells/Enemies/WallWorm.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Intrinsics.Arm;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleMaze.Maze.Cells.Enemies
+﻿namespace ConsoleMaze.Maze.Cells.Enemies
 {
     public class WallWorm : BaseEnemy
     {
